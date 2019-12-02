3d48caa234bcad12027bc839bee3cc4179f06786
xiaominzhao <xiaominzhao@dhcp-10-134-149-120.uawifi.arizona.edu>
2019-12-02 12:29:05 -0700

    f

--- src/view/View.java
+++ src/view/View.java
@@ -1,5 +1,33 @@
 package view;
+import java.util.Observable;
+import java.util.Observer;
 
-public class View {
-	
+import javafx.animation.TranslateTransition;
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class View extends Application implements Observer{
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+ 
+
 }
