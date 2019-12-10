1e780091c87f29fb15f7a8c419ce3a776957aa74
chendongzhe <chendongzhe@dhcp-10-134-173-15.uawifi.arizona.edu>
2019-12-09 18:57:18 -0700

    dz

--- src/view/GameStage.java
+++ src/view/GameStage.java
@@ -177,8 +177,11 @@ public class GameStage implements Observer {
 			}
 		}catch (Exception e) {
 			heal = model.getMap().getPlayer().getHealth();
-			
 			healL.setText(String.valueOf(heal));	
+			if(heal == 0) {
+				gameOver(stage);
+			}
+
 		}
 		
 		
