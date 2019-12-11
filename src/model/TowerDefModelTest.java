77bd5264a380003cebbd40d914eeb6bca72e3470
jiachenghe <jiachenghe@dhcp-10-132-150-119.uawifi.arizona.edu>
2019-12-10 22:08:37 -0700

    commit

---
+++ src/model/TowerDefModelTest.java
@@ -0,0 +1,28 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+public class TowerDefModelTest {
+	
+	
+	
+	@Test
+	public void testMap() {
+		
+	}
+	
+	@Test
+	public void testPlayer() {
+		
+	}
+	
+	@Test
+	public void testTowers() {
+		
+	}
+	
+	@Test
+	public void testMonsters() {
+		
+	}
+}
